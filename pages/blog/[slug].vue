a548f8898faeda73c96f65d9e804a963ea93cc27
Kokosalah45 <kokp.amged@gmail.com>
2023-04-22 13:54:25 +0200

    feature:back to blog link

--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -2,6 +2,9 @@
   <NuxtLayout :name="layout">
     <template #section>
       <div class="max-w-5xl text-white m-auto font-mono p-3">
+        <NuxtLink class="translate-y-12 underline text-green-lizard" to="/blog"
+          >Return to Main Page</NuxtLink
+        >
         <ContentDoc />
       </div>
     </template>
